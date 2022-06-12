7afc9d825f38ad0d5102381cceffe6bdd188af9a
Ramón Elías Toro <91712358+RAELTO@users.noreply.github.com>
2022-06-11 22:52:03 -0500

    UI updated - the account input and pin input are now working separately with the same number buttons,  also a screen was added. Now working in the backend logic

--- backend/js/main.js
+++ backend/js/main.js
@@ -4,24 +4,55 @@ var app = new Vue({
     el: '#app',
     data: {
         users: [
-            {accountn: '3333', pin: '3333', balance: 0},//admin
+            {accountn: '3333', pin: '3333', balance: 0, status: 1},//admin
             {accountn: '1234', pin: '1234', balance: 0, status: 1},
             {accountn: '5678', pin: '1234', balance: 0, status: 1},
             {accountn: '9012', pin: '1234', balance: 0, status: 1},
         ],
         tcash: [
-            {b50: 100000, amount: 50},
-            {b50: 50000, amount: 100},
-            {b20: 20000, amount: 100},
-            {b10: 10000, amount: 100}
+            {denom: 100000, amount: 50},
+            {denom: 50000, amount: 100},
+            {denom: 20000, amount: 100},
+            {denom: 10000, amount: 100}
         ],
         pin: '',
         accountn: '',
+        click_acc: false,
+        click_pin: false,
+        disp_marq: 1, //display marquee
+        disp_pininp: 0,
+        disp_accinp: 1,
+        uservw: 0,
     },
     methods: {
         buttons(n){
-            this.pin += n;
-            console.log(this.pin);
+            if (this.click_acc === true && this.accountn.length <= 3) {
+                this.accountn += n;
+            }else{
+                this.accountn = this.accountn;
+            }
+
+            if (this.click_pin === true && this.pin.length <=3) {
+                this.pin += n;
+            }else{
+                this.pin = this.pin;
+            }
+
+            if (this.accountn ) {
+                
+            }
+
         },
-    },
+        acc_input(){
+            this.click_acc = true;
+            this.click_pin = false;
+        },
+        pin_input(){
+            this.click_pin = true;
+            this.click_acc = false;
+        },
+        userview(){
+            this.uservw = 1;
+        }
+    }
 });
